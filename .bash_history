681b92db918e89bd68291466a25d7d774694896f
封斯旸 <2016011392@secoder.net>
2019-03-18 20:40:40 +0800

    20190318 20:40

---
+++ .bash_history
@@ -0,0 +1,15 @@
+sudo mkdir /pcshare
+sudo chmod 777 /pcshare
+sudo mount -t vboxsf ucore_os_lab /pcshare
+cd /ucore_os_lab
+sudo mkdir /mnt/shared
+sudo mkdir /pcshare
+sudo mount -t vboxsf ucore_os_lab /pcshare
+mkdir /mnt/pcshare
+sudo mkdir /mnt/pcshare
+mount -t vboxsf ucore_os_lab /mnt/pcshare
+sudo mount -t vboxsf ucore_os_lab /mnt/pcshare
+/mnt# ls
+sudo mount -t vboxsf ucore_os_lab /home/moocos
+
+sudo mount -t vboxsf ucore_os_lab /home/moocos/share
--- labcodes/lab1/kern/debug/kdebug.c
+++ labcodes/lab1/kern/debug/kdebug.c
@@ -288,19 +288,31 @@ read_eip(void) {
  * Note that, the length of ebp-chain is limited. In boot/bootasm.S, before jumping
  * to the kernel entry, the value of ebp has been set to zero, that's the boundary.
  * */
-void
-print_stackframe(void) {
+void print_stackframe(void) {
      /* LAB1 YOUR CODE : STEP 1 */
-     /* (1) call read_ebp() to get the value of ebp. the type is (uint32_t);
-      * (2) call read_eip() to get the value of eip. the type is (uint32_t);
-      * (3) from 0 .. STACKFRAME_DEPTH
-      *    (3.1) printf value of ebp, eip
-      *    (3.2) (uint32_t)calling arguments [0..4] = the contents in address (uint32_t)ebp +2 [0..4]
-      *    (3.3) cprintf("\n");
-      *    (3.4) call print_debuginfo(eip-1) to print the C calling function name and line number, etc.
-      *    (3.5) popup a calling stackframe
-      *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
-      *                   the calling funciton's ebp = ss:[ebp]
-      */
+    //(1) call read_ebp() to get the value of ebp. the type is (uint32_t);
+    uint32_t ebp = read_ebp();
+    //(2) call read_eip() to get the value of eip. the type is (uint32_t);
+    uint32_t eip = read_eip();
+    //(3) from 0 .. STACKFRAME_DEPTH
+    int i = 0;
+    for(; i < STACKFRAME_DEPTH && ebp != 0; i++){
+        //    (3.1) printf value of ebp, eip
+        cprintf("ebp = 0x%08x  eip = 0x%08x \n" , ebp , eip);
+        //    (3.2) (uint32_t)calling arguments [0..4] = the contents in address (uint32_t)ebp +2 [0..4]
+        uint32_t arg1 = *((uint32_t *)ebp + 2);
+        uint32_t arg2 = *((uint32_t *)ebp + 3);
+        uint32_t arg3 = *((uint32_t *)ebp + 4);
+        uint32_t arg4 = *((uint32_t *)ebp + 5);
+        cprintf("arg1 = 0x%08x arg2 = 0x%08x arg3 = 0x%08x arg4 = 0x%08x \n" , arg1 , arg2 , arg3 , arg4);
+        //    (3.3) cprintf("\n");
+        //    (3.4) call print_debuginfo(eip-1) to print the C calling function name and line number, etc.
+        print_debuginfo(eip - 1);
+        //    (3.5) popup a calling stackframe
+        //           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
+        //                   the calling funciton's ebp = ss:[ebp]
+        eip = *((uint32_t *)ebp+1);
+        ebp = *((uint32_t *)ebp);
+    }
 }
 
